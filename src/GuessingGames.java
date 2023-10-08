3fcc3a1d5e6d09c73a23c854858b63dd755f5c79
NeeruSisodia <143622011+NeeruSisodia@users.noreply.github.com>
2023-10-08 09:44:06 +0300

    add two comment

--- src/GuessingGames.java
+++ src/GuessingGames.java
@@ -11,11 +11,12 @@ public class GuessingGames {
             System.out.print("Guess the name: ");
             String userGuess = scanner.nextLine();
             attempts++;
-
+//How to check guess is right. 
             if (userGuess.equalsIgnoreCase(correctAnswer)) {
                 System.out.println("Congratulations! You guessed it right.");
                 System.out.println("Number of attempts: " + attempts);
                 isCorrect = true;
+                // If you want to quit the game.
             } else {
                 System.out.print("Incorrect guess. Do you want to quit? (yes/no): ");
                 String quitChoice = scanner.nextLine().toLowerCase();
